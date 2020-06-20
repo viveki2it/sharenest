d44c7e59520ab8b337d21227783abdec203ecd9d
Vivek <viveki4it@gmail.com>
2020-06-20 21:15:46 +0530

    done with changes

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -70,4 +70,5 @@ Rails.application.configure do
   #   :enable_starttls_auto => true
   # }
   config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
 end
